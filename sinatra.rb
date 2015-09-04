96d9285481e5da68db34c9f16d65a2c5a9565524
Kayla Danay <kayla.danay@gmail.com>
2015-09-04 11:46:37 -0400

    added routes

--- sinatra.rb
+++ sinatra.rb
@@ -1,2 +1,18 @@
 require 'sinatra'
 require 'mandrill'
+
+get '/' do 
+	erb :home
+end
+
+get '/menu' do
+	erb :menu
+end
+
+get '/order' do
+	erb :order
+end
+
+get '/reviews' do
+	erb :reviews
+end
