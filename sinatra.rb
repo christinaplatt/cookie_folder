c3a6aa3d17ca90382204ff94065b55618f962d0b
Kayla Danay <kayla.danay@gmail.com>
2015-10-12 16:38:44 -0400

    Fixed Mandrill

--- sinatra.rb
+++ sinatra.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 require 'mandrill'
 
 get '/' do
-  erb :home
+	erb :home
 end
 
 get '/home' do
-  erb :home
+	erb :home
 end
 
 get '/menu' do
@@ -22,3 +22,38 @@ end
 get '/reviews' do
 	erb :reviews
 end
+
+get '/thanks' do
+	puts "I HATE THIS"
+	erb :thanks
+end
+
+post '/order' do
+	puts "Params: #{params.inspect}"
+	
+	name = params["name"]
+	email = params["email"]
+	phone = params["phone"]
+	message = params["text"]
+	user_message = params["message"]
+	
+	puts "sending email!"
+	send_email(name, email, phone)
+	puts "GGGGGGGGGGGGGGGGGGGGGGGGDAJKSNJKASNFAS;NDFJKAS;NFJK;"
+	redirect '/thanks'
+end
+
+def send_email(name, email, phone)
+	
+	m = Mandrill::API.new
+	message = {  
+		:subject=> "Hello from Cookie Time!",  
+		:from_name=> "Tia, Liz, and Kayla",  
+		:text=>"Hi! You need cookies?",  
+		:to=>[{:email=> email, :name=> name}],  
+		:html=>"<html><h1>Hi, how are you?<strong>message</strong>, how are you?</h1> Your phone number is #{phone}</html>",  
+		:from_email=>"ksd44@drexel.edu"
+	}  
+		sending = m.messages.send message  
+		puts sending
+	end
